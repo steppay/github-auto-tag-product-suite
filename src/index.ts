88743b7f80eb0828773fc55f9e8e6144f7d8c3eb
Jihun <jihun@steppay.kr>
2023-03-21 11:29:17 +0900

    build: 태그 출력하기

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,27 @@ async function run() {
         const currentBranchName = await getCurrentBranchName()
 
         // push 한 브랜치명이 release 이고, develop 과 차이가 없으면 rc tag 를 부여한다.
-        if (currentBranchName === 'release') {
-            if (isBranchExist('develop') && hasDifferentCommits(currentBranchName, 'develop')) {
-                const tag = generateProductSuiteVersionByWeekNum() + '-rc'
-                await exec.exec('git', ['tag', tag])
-                await exec.exec('git', ['push', 'origin', tag])
-            }
+        if (
+            currentBranchName === 'release' &&
+            isBranchExist('develop') &&
+            hasDifferentCommits(currentBranchName, 'develop')
+        ) {
+            const tag = generateProductSuiteVersionByWeekNum() + '-rc'
+            console.log(tag)
+            await exec.exec('git', ['tag', tag])
+            await exec.exec('git', ['push', 'origin', tag])
         }
 
         // push 한 브랜치명이 main 이나 master 이고, release 와 차이가 없으면 release tag 를 부여한다.
-        if (currentBranchName === 'main' || currentBranchName === 'master') {
-            if (isBranchExist('release') && hasDifferentCommits(currentBranchName, 'release')) {
-                const tag = generateProductSuiteVersionByWeekNum() + '-release'
-                await exec.exec('git', ['tag', tag])
-                await exec.exec('git', ['push', 'origin', tag])
-            }
+        if (
+            (currentBranchName === 'main' || currentBranchName === 'master') &&
+            isBranchExist('release') &&
+            hasDifferentCommits(currentBranchName, 'release')
+        ) {
+            const tag = generateProductSuiteVersionByWeekNum() + '-release'
+            console.log(tag)
+            await exec.exec('git', ['tag', tag])
+            await exec.exec('git', ['push', 'origin', tag])
         }
     } catch (error) {
         core.setFailed(error.message)
